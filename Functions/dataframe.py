38e8cf4fa0992d63a90da2578f228e2afb4b7e16
Oscar Dyremyhr <oscar.dyremyhr@gmail.com>
2023-02-08 18:25:01 +0100

    Update dataframe.py

--- Functions/dataframe.py
+++ Functions/dataframe.py
@@ -1,3 +1,23 @@
 # Databricks notebook source
 def sum_two_numbers(a, b):
     print(a+b)
+    
+# Pivot by indicator, and fill missing values.
+def pivot_and_clean(pdf, fillna):
+    pdf["value"] = pd.to_numeric(pdf["value"])
+    pdf = pdf.fillna(fillna).pivot_table(
+        values="value", columns="indicator", index="date"
+    )
+    return pdf
+
+
+# Create column names that are compatible with Delta tables.
+def clean_spark_cols(pdf):
+    pdf.columns = pdf.columns.str.replace(" ", "_")
+    return pdf
+
+
+# Convert index to column (works with pandas API on Spark, too).
+def index_to_col(df, colname):
+    df[colname] = df.index
+    return df
