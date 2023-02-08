ea4902cea6b4105fb81c12a2c77c42571701b473
oscar.dyremyhr@gmail.com <oscar.dyremyhr@gmail.com>
2023-02-08 17:21:49 +0000

    init

---
+++ test notebook.py
@@ -0,0 +1,11 @@
+# Databricks notebook source
+# MAGIC %load_ext autoreload
+# MAGIC %autoreload 2
+
+# COMMAND ----------
+
+from Functions.dataframe import *
+
+# COMMAND ----------
+
+sum_two_numbers(1, 2)
